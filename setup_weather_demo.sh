7ca6441f9f529d029a6a3acdabc2c238f70d6130
Jamie Kuppel <jkuppel@splunk.com>
2019-10-11 13:56:08 -0700

    Added startup script that pulls the lastest changes from this repo every time the server starts.

---
+++ setup_weather_demo.sh
@@ -0,0 +1,11 @@
+#!/usr/bash
+
+if  [ ! -f "SplunkARWeatherDemoToolkit" ]; then
+  git clone "https://github.com/kingkupps/SplunkARWeatherDemoToolkit.git" || exit 1
+fi
+
+cd "SplunkARWeatherDemoToolkit" || exit 1
+git pull
+
+pip3 install -r requirements.txt
+python3 -m weatherdemo.server
